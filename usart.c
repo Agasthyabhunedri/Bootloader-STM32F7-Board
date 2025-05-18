beb504fd1d2f314ef3b162c7853e55f76ae8a6b0
Agasthyabhunedri <112179588+Agasthyabhunedri@users.noreply.github.com>
2025-05-18 10:43:06 -0500

    Create usart.c

---
+++ usart.c
@@ -0,0 +1,19 @@
+#include "usart.h"
+
+UART_HandleTypeDef huart2;
+
+void MX_USART2_UART_Init(void) {
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 115200;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    HAL_UART_Init(&huart2);
+}
+
+int UART_Receive(uint8_t* buffer, uint32_t len) {
+    return HAL_UART_Receive(&huart2, buffer, len, HAL_MAX_DELAY);
+}
